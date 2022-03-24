5cb064e72a327af1dfc05a8f668c6543299e9f25
Rammsnia <rumsnia00@mail.ru>
2022-03-25 00:50:44 +0400

    Labs
    Labs

---
+++ Лабы/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Лабораторные работы</title>
+    <link rel="stylesheet" href="/Styles/styles.css">
+    <script type="text/javascript" src="/scripts/scripts.js"></script>
+</head>
+<body>
+    <h1>Мои лабораторные работы</h1>
+    <a href="mldmlab1.php">Первая лабораторная работа по МЛиДМ<br></a>
+    <a href="mldmlab2.php">Вторая лабораторная работа по МЛиДМ</a>
+
+
+</body>
+</html>
